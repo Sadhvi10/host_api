44d40a42f31a92d6e2e2afbdfb44458ed94c18ad
Sadhvi Sudhir Pugaonkar <65889688+Sadhvi10@users.noreply.github.com>
2021-03-26 20:55:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,18 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+import flask
 
-# Creating a flask app and using it to instantiate a socket object
-app = Flask(__name__)
-socketio = SocketIO(app)
+app = flask.Flask(_name_)
+app.config["DEBUG"] = True
 
-# values['slider1'] and values['slider2'] store the current value of the sliders
-# This is done to prevent data loss on page reload by client.
-values = {
-    'slider1': 25,
-    'slider2': 0,
-}
 
-# Handler for default flask route
-# Using jinja template to render html along with slider value as input
-@app.route('/')
-def index():
-    return render_template('index.html',**values)
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-# Handler for a message recieved over 'connect' channel
-@socketio.on('connect')
-def test_connect():
-    emit('after connect',  {'data':'Lets dance'})
-    
-#Socket handler to deal with message over silder value changed
-@socketio.on('Slider value changed')
-def value_changed(message):
-    values[message['who']] = message['data']
-    emit('update value', message, broadcast=True)
-    print("SLider value changed")
-
-@app.route("/",methods=['GET'])
-def get_example():
-    response = jsonify(message="Simple server is running")
-    # Enable Access-Control-Allow-Origin
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
-
+@app.route('/slots', methods=['GET'])
+def locations():
+    return "(1,2)(4,3)(7,1)(2,6)"
 
 # Notice how socketio.run takes care of app instantiation as well.
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
 
