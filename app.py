dc1b66642967a0bf0fd422463f6b1335879d03fa
Sadhvi Sudhir Pugaonkar <65889688+Sadhvi10@users.noreply.github.com>
2021-03-26 23:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>AMVM</h1><p>Welcome</p>"
 
 @app.route('/slots', methods=['GET'])
 def locations():
     return "(1,2)(4,3)(7,1)(2,6)"
 
 app.run()
+
 
